006e798752412813746ffedfad268ebcdb3d8ca6
sun-28 <sunpahwa28@gmail.com>
2024-11-21 20:26:04 +0530

    api fixes

--- backend/controllers/submission.js
+++ backend/controllers/submission.js
@@ -3,9 +3,17 @@ const Test = require("../models/Test");
 const codeQueue = require("../util/runQueue");
 
 const submit = async (req, res) => {
-  const { code, language, testCases, socketId, testId, studentId } = req.body;
+  const { code, language, testCases, socketId, testId, studentId, questionId } =
+    req.body;
 
-  if (!code || !language || !testCases || !testId || !studentId) {
+  if (
+    !code ||
+    !language ||
+    !testCases ||
+    !testId ||
+    !studentId ||
+    !questionId
+  ) {
     return res.status(400).send({ error: "Missing required fields" });
   }
 
@@ -19,7 +27,34 @@ const submit = async (req, res) => {
 
     await submission.save();
 
-    // Enqueue the job with submission ID
+    const test = await Test.findById(testId);
+
+    if (!test) {
+      return res.status(404).send({ error: "Test not found" });
+    }
+
+    if (!test.submissions.has(studentId)) {
+      test.submissions.set(studentId, new Map());
+    }
+
+    const studentSubmissions = test.submissions.get(studentId);
+
+    if (!studentSubmissions.has(questionId)) {
+      studentSubmissions.set(questionId, {
+        submissions: [],
+        isAccepted: false,
+      });
+    }
+
+    const questionSubmissions = studentSubmissions.get(questionId);
+
+    questionSubmissions.submissions.push(submission._id);
+
+    studentSubmissions.set(questionId, questionSubmissions);
+    test.submissions.set(studentId, studentSubmissions);
+
+    await test.save();
+
     const job = await codeQueue.add({
       code,
       language,
@@ -27,6 +62,8 @@ const submit = async (req, res) => {
       socketId,
       testId,
       submissionId: submission._id,
+      studentId,
+      questionId,
     });
 
     return res.status(200).send({ jobId: job.id, status: "queued" });
